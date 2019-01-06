b878dc61fda7ce1227b7094ac70908fb4927bc73
Mohsen Bostan <mohsenbostandev@gmail.com>
2019-01-06 08:30:37 +0330

    Add Form Tag To Welcome Page Pricing Section

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -24,29 +24,45 @@
                     <a href="/register" class="cta">همین الان شروع کن</a>
                 </div>
                 <div class="col-md-12 plans">
-                    <div class="row justify-content-center align-items-center">
-                        <div class="col-md-4 plan-1">
-                            <h3>۱ ماه</h3>
-                            <img src="/images/home-page/idea.png" alt="IMG One">
-                            <h2>۵۰۰۰۰ ریال</h2>
-                            <span>فعالسازی همه امکانات</span>
-                            <a href="/plan" class="btn-buy-plan">خرید و فعالسازی</a>
+                    @if (\Session::has('fail'))
+                        <center>
+                            <div class="col-md-4">
+                                <div class="alert alert-danger text-center">
+                                    <p class="text-center">{{ \Session::get('fail') }}</p>
+                                </div>
+                                <br/>
+                            </div>
+                        </center>
+                    @endif
+                    <form action="{{ route('buyPlan') }}" method="post">
+                        @csrf
+                        <div class="row justify-content-center align-items-center">
+                            <div class="col-md-4 plan-1">
+                                <h3>۱ ماه</h3>
+                                <img src="/images/home-page/idea.png" alt="IMG One">
+                                <h2>20000 ریال</h2>
+                                <span>فعالسازی همه امکانات</span>
+                                <input type="hidden" name="type" value="1">
+                                <button type="submit" class="btn-buy-plan">خرید و فعالسازی</button>
+                            </div>
+                            <div class="col-md-4 plan-2">
+                                <h3>۱ سال</h3>
+                                <img src="/images/home-page/molecule.png" alt="IMG One">
+                                <h2>220000 ریال</h2>
+                                <span>فعالسازی همه امکانات</span>
+                                <input type="hidden" name="type" value="2">
+                                <button type="submit" class="btn-buy-plan">خرید و فعالسازی</button>
+                            </div>
+                            <div class="col-md-4 plan-3">
+                                <h3>یک بار برای همیشه</h3>
+                                <img src="/images/home-page/atom.png" alt="IMG One">
+                                <h2>600000 ریال</h2>
+                                <span>فعالسازی همه امکانات</span>
+                                <input type="hidden" name="type" value="3">
+                                <button type="submit" class="btn-buy-plan">خرید و فعالسازی</button>
+                            </div>
                         </div>
-                        <div class="col-md-4 plan-2">
-                            <h3>۱ سال</h3>
-                            <img src="/images/home-page/molecule.png" alt="IMG One">
-                            <h2>3۰۰۰۰ ریال</h2>
-                            <span>فعالسازی همه امکانات</span>
-                            <a href="/plan" class="btn-buy-plan">خرید و فعالسازی</a>
-                        </div>
-                        <div class="col-md-4 plan-3">
-                            <h3>یک بار برای همیشه</h3>
-                            <img src="/images/home-page/atom.png" alt="IMG One">
-                            <h2>8۰۰۰۰ ریال</h2>
-                            <span>فعالسازی همه امکانات</span>
-                            <a href="/plan" class="btn-buy-plan">خرید و فعالسازی</a>
-                        </div>
-                    </div>
+                    </form>
                 </div>
             </div>
         </div>
